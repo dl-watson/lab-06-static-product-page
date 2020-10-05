47259875832a7da405d959c1f652c512102dab1a
dl-watson <dlarkinwatson@gmail.com>
2020-10-05 14:24:15 -0700

    establish initial working infrastructure with tests passing for a render function

--- app.js
+++ app.js
@@ -2,4 +2,43 @@
 
 // initialize state
 
-// set event listeners to update state and DOM
+import { pokemon } from './obj-data.js';
+/*
+
+<h2>Mr. Cruel</h2>
+<p>fusion of Mr. Mime and Tentacruel</p>
+<img src="./assets/mrcruel.png" alt="mrcruel">
+<p>$12.99</p>
+<button>Add to Cart</button>
+
+*/
+
+export function renderPokemon(pokemon) {
+
+    const product = document.createElement('div');
+
+    const name = document.createElement('h2');
+    const description = document.createElement('p');
+    const image = document.createElement('img');
+    const price = document.createElement('p');
+    const button = document.createElement('button');
+
+    name.textContent = pokemon.name;
+    product.appendChild(name);
+
+    description.textContent = pokemon.description;
+    product.appendChild(description);
+
+    image.src = `./assets/${pokemon.id}.png`;
+    image.alt = pokemon.id;
+    product.appendChild(image);
+
+    price.textContent = `$${pokemon.price.toFixed(2)}`
+    product.appendChild(price);
+
+    button.textContent = "Add to Cart";
+    product.appendChild(button);
+
+    return product;
+
+};
