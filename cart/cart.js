d7a5c1ba15a377cc20e8aa068f8258f749b5fdaf
dl-watson <dlarkinwatson@gmail.com>
2020-10-06 21:03:41 -0700

    i learned how to use filter and forEach and excitedly replaced all my loops

--- cart/cart.js
+++ cart/cart.js
@@ -1,26 +1,19 @@
-// render table items with a for loop
-
 import { cart } from '../pokemon.js';
 import { renderTableRow } from './renderCartView.js';
 import { calcOrderTotal } from './cart-utils.js';
 
 
-// the following code assigns an anchor for rendered DOM nodes
-// it loops through the cart array and affixes each DOM node to its 
-// correct place in the HTML table
-
 const tableAnchor = document.querySelector('tbody');
 
-for (let i = 0; i < cart.length; i++) {
-    const item = cart[i];
 
+cart.forEach(item => {
     const tr = renderTableRow(item);
-
     tableAnchor.appendChild(tr);
+});
 
-}
 
 const total = calcOrderTotal(cart);
+
 
 const tdTotal = document.querySelector('#total');
 tdTotal.textContent = `$${total}`;
