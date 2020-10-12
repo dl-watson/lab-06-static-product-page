c1c76fd5600e0712d3b19b85d96bc9b509d2c270
dl-watson <dlarkinwatson@gmail.com>
2020-10-12 08:10:32 -0700

    trying a path fix

--- utils.js
+++ utils.js
@@ -1,13 +1,12 @@
-import { getPokemonFromLocalStorage } from './renderView.js';
-import { setCart } from './cart/cart-api.js';
-import { _pokemon } from './consts.js';
+import { getPokemonFromLocalStorage } from "../renderView.js";
+import { setCart } from "./cart/cart-api.js";
+import { _pokemon } from "./consts.js";
 
 export function addProduct(obj) {
+  const staticPokemon = getPokemonFromLocalStorage();
 
-    const staticPokemon = getPokemonFromLocalStorage();
-
-    // push this new object into our products array
-    staticPokemon.push(obj);
-    // stringify the result and set localStorage
-    setCart(_pokemon, staticPokemon);
+  // push this new object into our products array
+  staticPokemon.push(obj);
+  // stringify the result and set localStorage
+  setCart(_pokemon, staticPokemon);
 }
